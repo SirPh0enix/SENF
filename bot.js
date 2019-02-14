1ec0ce320796545328161b4250ea73e3a16e1b7d
SirPh0enix <47606250+SirPh0enix@users.noreply.github.com>
2019-02-14 14:21:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,10 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'owner') {
+    	message.channel.send('Der Owner ist Schwenn aka SHD');
+  	}
+ });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
